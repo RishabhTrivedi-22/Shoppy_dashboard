087baec7d27befb26705a0d3a32c40ff48c20125
RishabhTrivedi-22 <rishabhtrivedi695@gmail.com>
2024-07-19 17:31:00 +0530

    Final Reviewing done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import './index.css'
+import './index.css';
 import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
 
 ReactDOM.render(
-      <App />,
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
